f4c9c8725ee9b2015d0469774ea450c3bde7c7e1
archupatil-90 <73097336+archupatil-90@users.noreply.github.com>
2021-01-04 17:23:17 +0530

    Add files via upload

---
+++ digital.js
@@ -0,0 +1,10 @@
+let data=document. getElementsByTagName('img');
+console.log(data);
+let data1=document.links;
+console.log(data1);
+// let data3=document.getElementsByClassName("img").alt;
+// console.log(data3);
+
+let d1=document.querySelector(".img").alt.length;
+console.log(d1);
+
